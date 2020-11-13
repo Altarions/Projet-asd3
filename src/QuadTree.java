8b850d6fb3afd7081e655a3df0261c271c81d3d6
Salade <cyprien.garnier@orange.fr>
2020-11-13 10:46:29 +0100

    add méthode

---
+++ src/QuadTree.java
@@ -0,0 +1,19 @@
+
+public class QuadTree {
+
+	public QuadTree(ImagePNG img){}
+	
+	public compressDelta() {}
+	
+	public compressPhi() {
+		
+	}
+	
+	public toPng() {
+		
+	}
+	
+	public String toString() {
+		
+	}
+}
