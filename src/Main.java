71770827e5c7e1d98ffee93b2e44366657282e47
Salade <cyprien.garnier@orange.fr>
2020-11-17 15:55:04 +0100

    ajout fonction main test et QuadTree

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,65 @@
+import java.awt.Color;
+import java.io.File;
+
 
 public class Main {
+	
+    public static void main( String[] args )
+    {
+        try {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("helloWord");
-	}
+            ImagePNG png = new ImagePNG("./src/pngs/128-gnu.png");
+            ImagePNG pngNB = new ImagePNG(png); // une copie
+            ImagePNG pngR = new ImagePNG(png); // une autre copie
+
+            // 1. MODIFICATION DES COULEURS
+
+            Color col,nb,red;
+            int r,g,b,l;
+            for (int x=0 ; x<png.width(); x++)
+            {
+                for (int y=0 ; y<png.height() ; y++)
+                {
+                    // extraction de la couleur du pixel (x,y)
+                    col = png.getPixel(x,y);
+                    // modification de la couleur
+                    r = col.getRed();
+                    g = col.getGreen();
+                    b = col.getBlue();
+                    l = (r+g+b)/3;
+                    nb = new Color(l,l,l);
+                    red = new Color(l,0,0);
+                    // inscription de la nouvelle couleur des pixels (x,y)
+                    pngNB.setPixel(x,y,nb);
+                    pngR.setPixel(x,y,red);
+                }
+            }
+            png.save("./src/pngs/128-gnu.png");
+            pngNB.save("./src/pngs/128-gnu.png"+"NB");
+            pngR.save("./src/pngs/128-gnu.png"+"R");
+
+            // 2. COMPARAISON DES RÉSULTATS
+
+            // calcul des indices de similarité
+            double siNB = ImagePNG.computeEQM(png,pngNB);
+            double siR = ImagePNG.computeEQM(png,pngR);
+
+            // chargement des fichiers
+            File fic = new File("./src/pngs/128-gnu.png");
+            File ficNB =  new File("./src/pngs/128-gnu.png"+"NB");
+            File ficR =  new File("./src/pngs/128-gnu.png"+"R");
+
+            // rapport des tailles
+            double wNB = Math.ceil(10000.0*ficNB.length() / fic.length())/100.0;
+            double wR = Math.ceil(10000.0*ficR.length() / fic.length())/100.0;
+
+            // affichage des critères
+            System.out.println("NB: taille="+wNB+"% / qualité="+siNB+"%");
+            System.out.println("R: taille="+wR+"% / qualité="+siR+"%");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/QuadTree.java
+++ src/QuadTree.java
@@ -1,19 +1,44 @@
+import java.util.ArrayList;
 
 public class QuadTree {
+	
+	private ArrayList<QuadTree> listQuadTree;
+	private ImagePNG img;
 
-	public QuadTree(ImagePNG img){}
-	
-	public compressDelta() {}
-	
-	public compressPhi() {
+	public QuadTree(ImagePNG img){
+		this.img = img;
+		this.listQuadTree = constructQuadTree();
+	}
+	/**
+	 * @role:
+	 * @return:
+	 */
+	private ArrayList<QuadTree> constructQuadTree() {
+		ArrayList<QuadTree> list = new ArrayList<QuadTree>();
+		
+		if(img.width()>0) {
+			
+			for(int i=0; i<4; i++) {
+				
+			}
+		}
+		
+		
+		return list;
 		
 	}
 	
-	public toPng() {
+	public void compressDelta(Integer delta) {}
+	
+	public void compressPhi(Integer phi) {
+		
+	}
+	
+	public void toPng() {
 		
 	}
 	
 	public String toString() {
-		
+		return "";
 	}
 }
