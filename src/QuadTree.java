ff925caad4905852bc116fb4944b079f48546c53
Salade <cyprien.garnier@orange.fr>
2020-11-20 10:52:36 +0100

    toString

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 
 public class QuadTree {
 	
-	private ArrayList<QuadTree> listQuadTree;//filsNO,NE,SO,SE
+	private QuadTree filsNO, filsNE, filsSO, filsSE;
 	private ImagePNG img;
 	private Integer centerX, centerY, widthX; //centre du quadTree et largeur
+	private boolean vide;
 
 	public QuadTree(ImagePNG img){
 		
@@ -12,31 +13,33 @@ public class QuadTree {
 		this.centerX = img.width()/2;
 		this.centerY = img.width()/2;
 		this.widthX = img.width()/2;
-		this.listQuadTree = constructQuadTree();
+		this.vide = false;
+		constructQuadTree();
 	}
 	public QuadTree(ImagePNG img, Integer centerX, Integer centerY, Integer widthX){
 		this.centerX = centerX;
 		this.centerY = centerY;
 		this.widthX = widthX;
 		this.img = img;
-		this.listQuadTree = constructQuadTree();
+		this.vide = false;
+		constructQuadTree();
 	}
 	/**
 	 * @role:
 	 * @return:
 	 */
-	private ArrayList<QuadTree> constructQuadTree() {
-		ArrayList<QuadTree> list = new ArrayList<QuadTree>();
+	private void constructQuadTree() {
 		
-		if(img.width()>0 ) {
+		if(img.width()>1) {
 			
-			for(int i=0; i<4; i++) {
-				
-			}
+			this.filsNO = new QuadTree(this.img, this.centerX/2, this.centerY/2, this.widthX/2);
+			this.filsNE = new QuadTree(this.img, this.centerX/2+this.centerX, this.centerY/2, this.widthX/2);
+			this.filsSO = new QuadTree(this.img, this.centerX/2, this.centerY/2+this.centerY, this.widthX/2);
+			this.filsSE = new QuadTree(this.img, this.centerX/2+this.centerX, this.centerY/2+this.centerY, this.widthX/2);
+			
+		}else {
+			this.vide = true;
 		}
-		
-		
-		return list;
 		
 	}
 	
@@ -50,7 +53,12 @@ public class QuadTree {
 		
 	}
 	
-	public String toString() {
-		return "";
+	public void toString(QuadTree quadTree) {
+		if(quadTree.vide != false) {
+			if(quadTree.filsSE.vide != false ) {
+				toString(quadTree.filsNO);
+				
+			}
+		}	
 	}
 }
