9ba7197b0164bca1ddd13e72821ca88fadb7af47
Salade <cyprien.garnier@orange.fr>
2020-12-03 18:08:34 +0100

    002

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         	int choix = menu();
         			
         	
-        	if(choix>0 && choix <8) {
+        	if(choix>1 && choix <8 && png != null || choix == 1) {
                        
                 switch(choix){
      	           case 1: loadImg();break;
@@ -28,7 +28,7 @@ public class Main {
      	           
                 }
         	}else {
-        		System.out.println("Le choix n'existe pas !");
+        		System.out.println("Le choix n'existe pas ou vous devez charger une image!");
         	}
         	System.out.println("\n");
         	if(choix != 7) {
@@ -114,12 +114,16 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 	}
-	private static void savePng() {
-		// TODO Auto-generated method stub
+	private static void savePng() throws IOException {
+		
+		ImagePNG newPng = quadTree.toPNG(png, quadTree);
+		
+        newPng.save("./src/result/result.png");
+        System.out.println("Ajout réussi");
 		
 	}
 	private static void saveTxt() throws IOException {
-		File dataFile = new File("./src/txt/result.txt");
+		File dataFile = new File("./src/result/result.txt");
 		
 		if (!dataFile.exists()) {
 			dataFile.delete();
@@ -130,69 +134,12 @@ public class Main {
 		 bw.write(quadTree.toString("",quadTree));
 		 bw.close();
 
-		 System.out.println("Ajout réussi");
+		 System.out.println("/nAjout réussi");
 		
 	}
 	private static void compare() {
 		// TODO Auto-generated method stub
 		
 	}
-
-    
-    
-    /*
-    ImagePNG pngNB = new ImagePNG(png); // une copie
-    ImagePNG pngR = new ImagePNG(png); // une autre copie
-
-    // 1. MODIFICATION DES COULEURS
-
-    Color col,nb,red;
-    int r,g,b,l;
-    for (int x=0 ; x<png.width(); x++)
-    {
-        for (int y=0 ; y<png.height() ; y++)
-        {
-            // extraction de la couleur du pixel (x,y)
-            col = png.getPixel(x,y);
-            // modification de la couleur
-            r = col.getRed();
-            g = col.getGreen();
-            b = col.getBlue();
-            l = (r+g+b)/3;
-            nb = new Color(l,l,l);
-            red = new Color(l,0,0);
-            // inscription de la nouvelle couleur des pixels (x,y)
-            pngNB.setPixel(x,y,nb);
-            pngR.setPixel(x,y,red);
-        }
-    }
-    png.save("./src/pngs/128-gnu.png");
-    pngNB.save("./src/pngs/128-gnu.png"+"NB");
-    pngR.save("./src/pngs/128-gnu.png"+"R");
-
-    // 2. COMPARAISON DES R�SULTATS
-
-    // calcul des indices de similarit�
-    double siNB = ImagePNG.computeEQM(png,pngNB);
-    double siR = ImagePNG.computeEQM(png,pngR);
-
-    // chargement des fichiers
-    File fic = new File("./src/pngs/128-gnu.png");
-    File ficNB =  new File("./src/pngs/128-gnu.png"+"NB");
-    File ficR =  new File("./src/pngs/128-gnu.png"+"R");
-
-    // rapport des tailles
-    double wNB = Math.ceil(10000.0*ficNB.length() / fic.length())/100.0;
-    double wR = Math.ceil(10000.0*ficR.length() / fic.length())/100.0;
-
-    // affichage des critères
-
-    System.out.println("NB: taille="+wNB+"% / qualit�="+siNB+"%");
-    System.out.println("R: taille="+wR+"% / qualit�="+siR+"%");
-	
-
-    System.out.println("NB: taille="+wNB+"% / qualit�="+siNB+"%");
-    System.out.println("R: taille="+wR+"% / qualit�="+siR+"%");
-    */
 
 }
