9485bbc0e9656de897f553ec6980526a21c7b37f
Salade <cyprien.garnier@orange.fr>
2020-12-03 17:46:39 +0100

    sélecteur d'image

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         	int choix = menu();
         			
         	
-        	if(choix>0 && choix <7) {
+        	if(choix>0 && choix <8) {
                        
                 switch(choix){
      	           case 1: loadImg();break;
@@ -30,8 +30,10 @@ public class Main {
         	}else {
         		System.out.println("Le choix n'existe pas !");
         	}
-        	System.out.println("\n\n\n\n\n\n");
-        	main(args);
+        	System.out.println("\n");
+        	if(choix != 7) {
+        		main(args);
+        	}
         	
         } catch (Exception e) {
             e.printStackTrace();
@@ -47,6 +49,7 @@ public class Main {
     	System.out.println("	4: Sauvegarde le quadtree dans un fichier PNG");
     	System.out.println("	5: Sauvegarde la représentation textuelle du quadtree dans un fichier TXT");
     	System.out.println("	6: Donne les mesures comparative de deux fichiers images PNG");
+    	System.out.println("	7: Arreter le programme");
     	System.out.print("\nEntrez un choix : ");
     	
 		return sc.nextInt();
@@ -54,11 +57,53 @@ public class Main {
 
 	private static void loadImg() throws IOException {
 		
-		System.out.println("Entrez l'adresse du fichier");
-		//String nameFile = sc.next();	
-		png = new ImagePNG("./src/pngs/4.png");
-		quadTree = new QuadTree(png, png.width()/2, png.height()/2, png.width()/2);
-		System.out.println("Chargement réussi");
+		int choix;
+		String adress = "./src/pngs/";
+		
+		System.out.println("Chargement d'une image");
+    	System.out.println("	1: 1024-cube.png");
+    	System.out.println("	2: 128-gnu.png");
+    	System.out.println("	3: 16.png");
+    	System.out.println("	4: 2.png");
+    	System.out.println("	5: 256-trash.png");
+    	System.out.println("	6: 256-tux.png");
+    	System.out.println("	7: 32-tux.png");
+    	System.out.println("	8: 4.png");
+    	System.out.println("	9: 512-books.png");
+    	System.out.println("	10: 64-tuxette.png");
+    	System.out.println("	11: 8-tux.png");
+    	System.out.println("	12: i.png");
+    	System.out.print("\nEntrez un choix : ");
+    	
+    	choix = sc.nextInt();	
+    	
+    	if(choix>0 && choix <13) {
+                   
+    		switch(choix){
+	           case 1: adress += "1024-cube.png";break;
+	           case 2: adress += "128-gnu.png";break;
+	           case 3: adress += "16.png";break;
+	           case 4: adress += "2.png";break;
+	           case 5: adress += "256-trash.png";break;
+	           case 6: adress += "256-tux.png";break;
+	           case 7: adress += "32-tux.png";break;
+	           case 8: adress += "4.png";break;
+	           case 9: adress += "512-books.png";break;
+	           case 10: adress += "64-tuxette.png";break;
+	           case 11: adress += "8-tux.png";break;
+	           case 12: adress += "i.png";break;
+	           
+            }
+    		
+    		png = new ImagePNG(adress);
+    		quadTree = new QuadTree(png, png.width()/2, png.height()/2, png.width()/2);
+    		System.out.println("Chargement réussi");
+    		
+    	}else {
+    		System.out.println("\nLe choix n'existe pas !\n");
+    		loadImg();
+    	}
+		
 		
 	}
 	private static void delta() {
